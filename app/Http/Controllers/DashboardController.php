db651e9942887bbfb4dbe2d3f78f4686dc7e30d2
marininik <m.nikolli@lswr.it>
2022-07-07 12:47:40 +0200

    .

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -11,16 +11,8 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        // $retrieved = User::all();
+
         $retrieved = User::with('roles', 'permissions')->get();
-        // $permission = User::all()->map(fn($us) => $us->getPermissionsViaRoles());
-        // $permission = User::map(fn($us) => $us->getPermissionsViaRoles())->get();
-
-        // $persmission = Permission::get();
-
-        // dd($permission);
-        // dd($retrieved);
-
 
         $user = (auth()->user() === 'Super-Admin') ? null : $retrieved;
 
@@ -31,29 +23,14 @@ class DashboardController extends Controller
 
     public function edit(User $user)
     {
-
         // $retrieved = $user->getAllPermissions();
         $retrieved = $user->roles->pluck('name', 'id');
         $allPermission = Role::get();
 
         // dd($retrieved);
-
         return Inertia::render('Dashboard/Edit', [
             'role' => $retrieved,
             'permission' => $allPermission,
-                // 'whatnot' => $allPermission
         ]);
     }
-
-
-    // 'name' => $user->name,
-    // 'email' => $user->email,
-    // 'phone' => $user->phone,
-    // 'address' => $user->address,
-    // 'city' => $user->city,
-    // 'region' => $user->region,
-    // 'country' => $user->country,
-    // 'postal_code' => $user->postal_code,
-    // 'deleted_at' => $user->deleted_at,
-    // 'contacts' => $user->contacts()->orderByName()->get()->map->only('id', 'name', 'city', 'phone'),
 }
